e0f563692509670502cc14946b4feab04d4bff96
Peter Zander <16868247+PeterZander@users.noreply.github.com>
2020-02-07 12:51:03 +0100

    Refactored SSUHost. Regions -> Partial files.

---
+++ I2PCore/Transport/SSU/SSUHost.Sockets.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using I2PCore.Utils;
+
+namespace I2PCore.Transport.SSU
+{
+    public partial class SSUHost
+    {
+        Socket MySocket;
+        EndPoint RemoteEP;
+        EndPoint LocalEP;
+
+        byte[] ReceiveBuf = new byte[65536];
+        internal SendBufferPool SendBuffers = new SendBufferPool();
+
+        public void NetworkSettingsChanged()
+        {
+            MySocket.Close( 1 );
+
+            CreateSocket();
+        }
+
+        private void CreateSocket()
+        {
+            IPAddress local = MyRouterContext.Address;
+            LocalEP = new IPEndPoint( local, MyRouterContext.UDPPort );
+            RemoteEP = LocalEP;
+
+            var newsocket = new Socket( AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp );
+            newsocket.SetSocketOption( SocketOptionLevel.Socket, SocketOptionName.ReceiveBuffer, 65536 );
+            newsocket.SetSocketOption( SocketOptionLevel.Socket, SocketOptionName.SendBuffer, 65536 );
+            newsocket.Bind( LocalEP );
+
+            var oldsocket = MySocket;
+            MySocket = newsocket;
+            if ( oldsocket != null ) oldsocket.Close();
+
+            Logging.LogInformation( $"SSUHost: Running with new network settings. " +
+                $"{local}:{MyRouterContext.UDPPort} ({MyRouterContext.ExtAddress})" );
+        }
+
+        private void ReceiveCallback( IAsyncResult ar )
+        {
+            SSUSession session = null;
+            try
+            {
+                EndPoint ep = RemoteEP;
+                var size = MySocket.EndReceiveFrom( ar, ref ep );
+
+                if ( ep.AddressFamily != AddressFamily.InterNetwork ) return; // TODO: Add IPV6
+
+                if ( size <= 37 )
+                {
+#if LOG_ALL_TRANSPORT
+                    Logging.LogTransport( string.Format( "SSU Recv: {0} bytes [0x{0:X}] from {1} (hole punch, ignored)", size, ep ) );
+#endif
+                    return;
+                }
+
+                var key = (IPEndPoint)ep;
+
+#if LOG_ALL_TRANSPORT
+                Logging.LogTransport( string.Format( "SSU Recv: {0} bytes [0x{0:X}] from {1}", size, ep ) );
+#endif
+
+                lock ( Sessions )
+                {
+                    if ( !Sessions.ContainsKey( key ) )
+                    {
+                        if ( IPFilter.IsFiltered( ( (IPEndPoint)ep ).Address ) )
+                        {
+                            Logging.LogTransport( $"SSUHost ReceiveCallback: IPAddress {key} is blocked. {size} bytes." );
+                            return;
+                        }
+
+                        ++IncommingConnectionAttempts;
+
+                        session = new SSUSession( this, (IPEndPoint)ep, MTUProvider, MyRouterContext );
+                        Sessions[key] = session;
+                        Logging.LogTransport( $"SSUHost: incoming connection " +
+                            $"{session.DebugId} from {key} created." );
+                        NeedCpu( session );
+                        ConnectionCreated?.Invoke( session );
+                    }
+                    else
+                    {
+                        session = Sessions[key];
+                    }
+                }
+
+                var localbuffer = BufRefLen.Clone( ReceiveBuf, 0, size );
+
+#if DEBUG
+                Stopwatch Stopwatch1 = new Stopwatch();
+                Stopwatch1.Start();
+#endif
+                try
+                {
+                    session.Receive( localbuffer );
+                }
+                catch ( ThreadAbortException taex )
+                {
+                    AddFailedSession( session );
+                    Logging.Log( taex );
+                }
+                catch ( ThreadInterruptedException tiex )
+                {
+                    AddFailedSession( session );
+                    Logging.Log( tiex );
+                }
+                catch ( ChecksumFailureException cfex )
+                {
+                    AddFailedSession( session );
+                    Logging.Log( cfex );
+                }
+                catch ( SignatureCheckFailureException scex )
+                {
+                    AddFailedSession( session );
+                    Logging.Log( scex );
+                }
+                catch ( FailedToConnectException fcex )
+                {
+                    AddFailedSession( session );
+#if LOG_ALL_TRANSPORT
+            Logging.LogTransport( fcex );
+#endif
+                    if ( session != null )
+                    {
+                        NetDb.Inst.Statistics.FailedToConnect( session.RemoteRouterIdentity.IdentHash );
+                        session.RaiseException( fcex );
+                    }
+                }
+#if DEBUG
+                Stopwatch1.Stop();
+                if ( Stopwatch1.ElapsedMilliseconds > SessionCallWarningLevelMilliseconds )
+                {
+                    Logging.LogTransport(
+                        $"SSUHost ReceiveCallback: WARNING Session {session} used {Stopwatch1.ElapsedMilliseconds}ms cpu." );
+                }
+#endif
+            }
+            catch ( Exception ex )
+            {
+                AddFailedSession( session );
+                Logging.Log( ex );
+
+                if ( session != null && session.RemoteRouterIdentity != null && NetDb.Inst != null )
+                {
+                    NetDb.Inst.Statistics.DestinationInformationFaulty( session.RemoteRouterIdentity.IdentHash );
+                    session.RaiseException( ex );
+                }
+            }
+            finally
+            {
+                RemoteEP = LocalEP;
+                MySocket.BeginReceiveFrom( ReceiveBuf, 0, ReceiveBuf.Length, SocketFlags.None, ref RemoteEP,
+                    new AsyncCallback( ReceiveCallback ), MySocket );
+            }
+        }
+
+        internal void Send( IPEndPoint ep, BufLen data )
+        {
+            MySocket.BeginSendTo( data.BaseArray, data.BaseArrayOffset, data.Length, SocketFlags.None, ep, new AsyncCallback( SendCallback ), data );
+#if LOG_ALL_TRANSPORT
+            Logging.LogTransport( $"SSU Sent: {data.Length} bytes [0x{0:X}] to {ep}" );
+#endif
+        }
+
+        private void SendCallback( IAsyncResult ar )
+        {
+            try
+            {
+                MySocket.EndSendTo( ar );
+            }
+            catch ( Exception ex )
+            {
+                Logging.Log( ex );
+            }
+            finally
+            {
+                SendBuffers.Push( (BufLen)ar.AsyncState );
+            }
+        }
+
+        DecayingIPBlockFilter IPFilter = new DecayingIPBlockFilter();
+        public int BlockedIPCount { get { return IPFilter.Count; } }
+
+        void ReportEPProblem( IPEndPoint ep )
+        {
+            IPFilter.ReportProblem( ep.Address );
+        }
+
+    }
+}
---
+++ I2PCore/Transport/SSU/SSUHost.Utilities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using I2PCore.Utils;
+
+namespace I2PCore.Transport.SSU
+{
+    public partial class SSUHost
+    {
+        static readonly DateTime SSURefDateTime = new DateTime( 1970, 1, 1 );
+        public static uint SSUTime( DateTime dt ) { return (uint)( ( dt - SSURefDateTime ).TotalSeconds ); }
+        public static DateTime SSUDateTime( uint sec ) { return SSURefDateTime.AddSeconds( sec ); }
+
+        class EPComparer : IEqualityComparer<IPEndPoint>
+        {
+            public bool Equals( IPEndPoint x, IPEndPoint y )
+            {
+                if ( x == null && y == null ) return false;
+                if ( x == null || y == null ) return false;
+                if ( ReferenceEquals( x, y ) ) return true;
+                return ( BufUtils.Equal( x.Address.GetAddressBytes(), y.Address.GetAddressBytes() ) && x.Port == y.Port );
+            }
+
+            public int GetHashCode( IPEndPoint obj )
+            {
+                return obj.Address.GetAddressBytes().ComputeHash() ^ obj.Port;
+            }
+        }
+    }
+}
